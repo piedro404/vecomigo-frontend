1f574785acf2cf5c45ebd7c1ee8d02a13310ebac
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2026-03-18 19:38:53 -0300

    feat: remover importação não utilizada do componente Link em Room.tsx

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useRoom } from "@hooks/useRoom";
 import { useVideo } from "@hooks/useVideo";
 
